aeb0f3ebf061ca7b876ff374a74c3d46c45a9eff
jonathan.deng <dengjonathan@gmail.com>
2016-09-16 11:49:13 -0400

    updated title with random gen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,6 +13,44 @@ $( document ).ready(function() {
       return Math.round(parseFloat(value));
     });
 
+  // generate random pithy saying
+  var genSaying = function() {
+    var sayings = [
+      ' drives a Subie hatchback',
+      '\'s dog has really short legs and a big head',
+      ' builds the future',
+      ' dreams big dreams',
+      ' can\'t smell things outside a five foot radius',
+      ' likes nature',
+      ' wakes up early and goes to bed late',
+      ' orders two lunches',
+      ', frankly, is a little low-brow',
+      'tries, to varying results, to make friends',
+      ' has the legs of a sloth, the will of a wolf',
+      ' thinks flat is better than nested',
+      ' tries to minimize side effects and be explicit',
+      ' thinks namespaces are a honking good idea',
+      ' is kind of a brogrammer',
+      ' likes reading Raymond Carver',
+      ' laughs really awkwardly',
+      ' openly admits to liking G-Eazy',
+      ' dates a lawyer, but is still cool, really',
+      ' collects cotton sweatshirts',
+      ' hacks the world',
+      ' is a fumbler with RegEx',
+      ' thinks practicality beats purity',
+      ' is a JavaScript nerd',
+      ' is not afraid to go where things are hacky',
+      ' is making this 😜 face',
+      ' would rather be 🏊'
+
+    ];
+    var index = Math.floor(Math.random() * sayings.length);
+    console.log(sayings[index]);
+    $('.pithy-saying').text(sayings[index]);
+  }
+    $('.pithy-saying').click(genSaying);
+    genSaying();
 });
 
 
